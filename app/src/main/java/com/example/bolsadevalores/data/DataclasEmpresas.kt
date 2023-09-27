318d8ae057eb0223b9f80ee415fc607dc48df8fc
JoseArtigas <joseartigasblasco@gmail.com>
2023-09-27 09:24:26 +0200

    data

---
+++ app/src/main/java/com/example/bolsadevalores/data/DataclasEmpresas.kt
@@ -0,0 +1,8 @@
+package com.example.bolsadevalores.data
+
+class DataclasEmpresas (
+    var nombre:String,
+    var cotizacion: Double,
+    var incremento: Double,
+    var porcentaje: Double
+)
---
+++ app/src/main/java/com/example/bolsadevalores/data/EmpresasProvider.kt
@@ -0,0 +1,47 @@
+package com.example.bolsadevalores.data
+
+class EmpresasProvider {
+
+    companion object{
+
+        var empresaslist = listOf<DataclasEmpresas>(
+
+            DataclasEmpresas("ACCIONA", 157.20, 0.95, +0.61),
+            DataclasEmpresas("ACCIONA ENERGIA", 30.78, 0.00, 0.00),
+            DataclasEmpresas("ACERINOX", 9.96, -0.02, -0.32),
+            DataclasEmpresas("AENA", 144.35, -0.10, -0.03),
+            DataclasEmpresas("AMADEUS", 67.78, -0.36, -0.53),
+            DataclasEmpresas("ACELORMITTAL", 24.60, -0.13, -0.53),
+            DataclasEmpresas("BANKINTER", 5.49, 0.02, +0.33),
+            DataclasEmpresas("BBVA", 6.76, 0.1, +0.18),
+            DataclasEmpresas("CAIXABANK", 3.68, 0.05, +1.43),
+            DataclasEmpresas("CELLNEX TELECOM", 35.88, 0.66, +0.24),
+            DataclasEmpresas("ENAGAS", 17.76, 0.03, +0.17),
+            DataclasEmpresas("ENDESA", 21.12, 0.01, +0.05),
+            DataclasEmpresas("FLUIDRA", 16.75, 0.84, +0.14),
+            DataclasEmpresas("GRIFOLS", 11.45, -0.22, -0.03),
+            DataclasEmpresas("IAG (IBERIA)", 1.82, -0.01, -0.33),
+            DataclasEmpresas("IBERDROLA", 11.69, 0.04, +0.30),
+            DataclasEmpresas("INDITEX", 33.81, -0.12, -0.35),
+            DataclasEmpresas("INDRA", 11.12, -0.05, -0.45),
+            DataclasEmpresas("INM. COLONIAL", 5.43, 0.02, +0.37),
+            DataclasEmpresas("LABORATORIOS FARMA", 40.96, -0.14, -0.34),
+            DataclasEmpresas("LOGISTA", 24.06, 0.06, +0.25),
+            DataclasEmpresas("MAPFRE", 1.77, 0.01, +0.28),
+            DataclasEmpresas("MELIA HOTELS INTL", 6.23, 0.00, 0.00),
+            DataclasEmpresas("MERLIN PROP", 7.58, 0.03, +0.33),
+            DataclasEmpresas("NATURGY", 26.54, 0.10, +0.38),
+            DataclasEmpresas("REE", 15.93, 0.07, +0.44),
+            DataclasEmpresas("REPSOL", 12.94, -0.15, -1.15),
+            DataclasEmpresas("SABADELL", 1.02, 0.01, +1.04),
+            DataclasEmpresas("SACYR", 3.02, 0.01, +0.39),
+            DataclasEmpresas("SANTANDER", 157.20, 0.10, +3.10),
+            DataclasEmpresas("SOLARIA ENERGIA", 13.83, 0.16, +1.17),
+            DataclasEmpresas("TELEFONICA", 3.60, -0.01, -0.28),
+            DataclasEmpresas("UNICAJA BANCO", 0.92, 0.01, +0.10),
+
+            )
+
+    }
+
+}
